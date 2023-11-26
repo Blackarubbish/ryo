7ac9cfad3932b1c4f3696c49e2b80bdd69c334ed
shunlin <shunlin.li@qq.com>
2023-11-26 17:34:15 +0800

    feat: 添加husky

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,20 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-case': [2, 'always', ['lower-case', 'upper-case']],
-    'type-enum': [2, 'always',['feat', 'fix', 'docs','style','refactor','perf','test', 'chore', 'revert']]
-  }
-}
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'revert',
+      ],
+    ],
+  },
+};
